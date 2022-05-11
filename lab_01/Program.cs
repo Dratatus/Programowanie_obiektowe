4abd2de089b1c3080474b1d6a677891976c1b5df
Daniel Partatus <Daniel.Partatus@microsoft.wsei.edu.pl>
2022-05-11 21:26:32 +0200

    Add project files.

---
+++ lab_01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace prog_obj_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+
+    class Ulamek
+    {
+        Ulamek ulamek1 = new Ulamek(5, 3);
+
+        private int mianownik { get; set; }
+        private int licznik { get; set; }
+
+
+
+        public Ulamek()
+        {
+
+        }
+
+
+        public Ulamek(int test, int test2)
+        {
+            test = 2;
+            test2 = 4;
+        }
+
+        public Ulamek(Ulamek kopiaUlamka)
+        {
+            ulamek1 = kopiaUlamka.ulamek1;
+        }
+
+
+        public static Ulamek operator +(Ulamek licz, Ulamek mian)
+        {
+            return new Ulamek(licz.)
+        }
+
+
+    }
+
+
+
+}
